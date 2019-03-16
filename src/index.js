9a5af8161b67ea78983632d405c1a7d76872aff8
Jared <me@jaredp.co.uk>
2019-03-16 10:08:41 +0000

    dtls socket setup

--- src/index.js
+++ src/index.js
@@ -1,5 +1,7 @@
 require('dotenv').config()
+const {dtls} = require('node-dtls-client')
 const fs = require('fs')
+const hexToBinary = require('hex-to-binary')
 const rp = require('request-promise')
 const semver = require('semver')
 
@@ -9,11 +11,56 @@ class HueSync {
     this.bridgeUri = `http://${this.bridgeIp}`
     this.auth = null
     this.config = null
+    this.dtlsSocket = null
+    this.interval = null
     this.groupId = null
     this.state = null
   }
 
+  async stop () {
+    if (this.interval) {
+      clearInterval(this.interval)
+    }
+
+    if (this.groupId) {
+      await rp({
+        uri: `${this.bridgeUri}/api/${this.auth.username}/groups/${this.groupId}`,
+        method: 'PUT',
+        json: true,
+        body: {
+          stream: {
+            active: false
+          }
+        }
+      })
+    }
+
+    if (this.dtlsSocket) {
+      this.dtlsSocket.close()
+    }
+  }
+
   async start () {
+    if (!await this.setupHue()) {
+      return
+    }
+
+    try {
+      await this.setupDtlsSocket()
+    } catch (e) {
+      console.log('error', e)
+
+      return
+    }
+
+    const msg = this.createLightingBuffer()
+
+    this.interval = setInterval(() => {
+      this.dtlsSocket.send(msg)
+    }, 20)
+  }
+
+  async setupHue () {
     this.config = await rp({
       uri: `${this.bridgeUri}/api/config`,
       json: true
@@ -22,7 +69,7 @@ class HueSync {
     if (semver.lt(this.config.apiversion, '1.22.0')) {
       console.error('Your bridge must be running at least version 1.22')
 
-      return
+      return false
     }
 
     await this.authenticate()
@@ -30,7 +77,7 @@ class HueSync {
     if (!this.auth) {
       console.error('Unable to retrieve authentication string')
 
-      return
+      return false
     }
 
     this.groupId = Object.keys(this.state.groups).find(
@@ -46,7 +93,7 @@ class HueSync {
         }, '')
       )
 
-      return
+      return false
     }
 
     const startStream = await rp({
@@ -63,10 +110,32 @@ class HueSync {
     if (this.responseIsError(startStream)) {
       console.error('Failed to start streaming')
 
-      return
+      return false
     }
 
-    console.log('And we are go!')
+    return true
+  }
+
+  async setupDtlsSocket () {
+    return new Promise((resolve, reject) => {
+      const psk = {}
+
+      psk[this.auth.username] = Buffer.from(this.auth.clientkey, 'hex')
+
+      this.dtlsSocket = dtls
+        .createSocket({
+          type: 'udp4',
+          address: this.bridgeIp,
+          port: 2100,
+          psk,
+          timeout: 1000,
+          ciphers: ['TLS_PSK_WITH_AES_128_GCM_SHA256']
+        }).on('connected', () => {
+          resolve()
+        }).on('error', (e) => {
+          reject(e)
+        })
+    })
   }
 
   async authenticate () {
@@ -109,9 +178,54 @@ class HueSync {
     fs.writeFileSync('auth.json', JSON.stringify(res[0].success))
   }
 
+  createLightingBuffer () {
+    const lights = []
+
+    this.state.groups[this.groupId].lights.forEach(light => {
+      const lightId = light.padStart(2, '0').split('')
+
+      lights.push(Buffer.from([
+        0x00, lightId[0], lightId[1],
+        0xff, 0xff, 0x00, 0x00, 0x00, 0x00 // red
+        // 0x00, 0x00, 0x00, 0x00, 0xff, 0xff, // blue
+        // 0x0c, 0x0d, 0x00, 0x03, 0x0d, 0x0b, // purple
+      ]))
+    })
+
+    return Buffer.concat([
+      Buffer.from('HueStream', 'ascii'),
+      Buffer.from([
+        // Version
+        0x01, 0x00,
+        // Sequence ID
+        0x00,
+        // Reserved
+        0x00, 0x00,
+        // Color space
+        0x00,
+        // Reserved
+        0x00
+      ]),
+      ...lights
+    ])
+  }
+
   responseIsError (res) {
     return typeof res[0] !== 'undefined' && typeof res[0].error !== 'undefined'
   }
 }
 
-new HueSync().start()
+const hue = new HueSync()
+hue.start()
+
+process.on('SIGINT', () => {
+  if (hue) {
+    hue.stop().then(() => {
+      process.exit(0)
+    })
+
+    return
+  }
+
+  process.exit(0)
+})
